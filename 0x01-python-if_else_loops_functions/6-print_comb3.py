32c6300337fbb70e71846250cb28f080b4b3e506
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-01 17:25:49 -0500

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for first in range(0, 8):
+    for second in range(0, 10):
+        if first < second:
+                print("{}{}" .format(first, second), end=", ")
+print("89")
