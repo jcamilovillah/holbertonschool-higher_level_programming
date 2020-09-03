743fd00851756b0730f22d646377c541b0cf401a
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-03 17:25:03 -0500

    no arguments condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d} : {:s}".format(i, sys.argv[i]))
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{} : {}".format(i, sys.argv[i]))
