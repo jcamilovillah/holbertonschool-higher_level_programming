744e2a045aa1e90dbd624a96f2ec75385a17f02a
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-03 17:43:53 -0500

    print message for argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
+        if len(sys.argv) == 2:
+            print("1 : argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
             print("{} : {}".format(i, sys.argv[i]))
