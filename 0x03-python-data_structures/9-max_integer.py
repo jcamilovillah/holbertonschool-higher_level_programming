712c61ecbf746e821a7a9cff3e72daa343dce376
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-08 23:01:03 -0500

    Maxi

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (len(my_list) == 0):
-        return None
-    maxi = my_list[0]
-    for i in my_list:
-        if(i > maxi):
-            maxi = i
-    return maxi
+    if (my_list):
+        maxi = my_list[0]
+        for i in my_list:
+            if(i > maxi):
+                maxi = i
+        return maxi
