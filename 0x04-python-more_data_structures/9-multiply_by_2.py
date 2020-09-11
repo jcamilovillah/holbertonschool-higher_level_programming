a8fa4cd2e4621c9e8c8b4e439f030f4051e692d1
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-10 23:59:33 -0500

    multy by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for i, j in a_dictionary.items():
+        new_dictionary[i] = j * 2
+    return new_dictionary
+    
