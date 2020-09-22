60a6a009604b2163a0ce4f29b1d29605766d7e3c
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-21 22:46:32 -0500

    pass

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        sum = 0
-        for i in range(0, x):
-            if type(my_list[i]) is int:
-                print("{}".format(my_list[i]), end="")
-                sum += 1
-        print()
-        return sum
-    except (TypeError, ValueError):
-        pass
+    sum = 0
+    for z in range(x):
+        try:
+            print("{:d}".format(my_list[z]), end="")
+            sum += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return sum
