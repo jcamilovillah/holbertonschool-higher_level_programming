5c7605d6a6e4945b9647b178176d331712887073
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-21 23:47:47 -0500

    div only integers

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    k = 0
+    for i in range(0, list_length):
+        try:
+            k = my_list_1[i]/my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(k)
+    return new_list
