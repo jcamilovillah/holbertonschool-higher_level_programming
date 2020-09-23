37bc26f4db0b0d32b992a5f0bed365bb30221586
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-22 21:22:13 -0500

    method update value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class that define as the point zero"""
+
+
+class Square:
+    """Class square"""
+    __size = 0
+
+    def __init__(self, size=0):
+        """Private size"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        """ define size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Define area"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
