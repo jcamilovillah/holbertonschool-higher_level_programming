6a06461785f9a61295899a6e16f385863e7d6f25
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-22 19:41:46 -0500

    attribute private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class taht define as the point zero"""
+
+
+class Square:
+    """Class square"""
+    __size = 0
+
+    def __init__(self, size):
+        """Private size"""
+        self.__size = size
