b57f69532989483845bd6b207eb1bec392d87971
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-25 10:52:50 -0500

    doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function"""
+
+
+def add_integer(a, b=98):
+    """sum two integers
+    args:
+        a: number
+        b: number
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Python 07 - Test Driven Development
