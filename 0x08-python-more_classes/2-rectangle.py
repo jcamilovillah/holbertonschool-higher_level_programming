23f711051c2298090a553783f995e4ee3aa2a643
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-28 19:11:51 -0500

    Error 1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,40 +4,38 @@
 
 class Rectangle:
     """Rectangle class"""
-    __width = 0
-    __height = 0
 
     def __init__(self, width=0, height=0):
         """init"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """define width"""
-        return self.width
+        """width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """set width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
+        """width setter"""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
-        """define height"""
-        return self.height
+        """height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """set height"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
+        """height setter"""
+        if type(value) != int:
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
