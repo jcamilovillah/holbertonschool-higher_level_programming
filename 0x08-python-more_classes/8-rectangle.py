51aa6d64eba298271c8628cd5949073aa7f3a2ef
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-28 21:33:47 -0500

    validation

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -73,12 +73,13 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """more bigger than"""
         rect1 = rect_1.area()
         rect2 = rect_2.area()
 
-        if type(rect_1) is not Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect1 >= rect2:
             return rect_1
