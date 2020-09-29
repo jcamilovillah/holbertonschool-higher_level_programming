6cd6c88d6b3e9bee0f4ae8734af8bc97129048d8
jcamilovillah <villahjuancamilo@gmail.com>
2020-09-29 18:39:05 -0500

    advance

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    count = 0
-    if hasattr(magic_string, 'n'):
-        count = getattr(magic_string, "n")
-    setattr(magic_string, "n", count)
-    return "Holberton" * magic_string.n
+    setattr(magic_string, "n", getattr(magic_string, "n", -1) + 1)
+    return "Holberton" + ", Holberton" * magic_string.n
