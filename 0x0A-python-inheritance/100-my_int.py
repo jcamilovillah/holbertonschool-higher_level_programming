483fa9cfce4cbc9bc60f378d3080f3c712d675d4
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-06 23:48:14 -0500

    advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""that inherits from int"""
+"""This module creates a Class"""
 
 
 class MyInt(int):
-    """Class"""
-    def __init__(self, num):
-        self.num = str(num)
+    """
+    Class MyInt
+        :param int: Inherits from int
+    """
+    def __init__(self, number):
+        self.number = number
+
+    def __eq__(self, other):
+        return self.number != other
+
+    def __ne__(self, other):
+        return other == self.number
