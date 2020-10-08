18ee58583113b5a3d1697f9089c9f1add565d5e2
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-07 22:39:06 -0500

    class student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age =age
+    
+    def to_json(self):
+        """dict"""
+        return self.__dict__
