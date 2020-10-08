b13f15712db692734b5ccc95558c4ef6210eea6d
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-07 20:14:34 -0500

    r

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,5 +4,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
