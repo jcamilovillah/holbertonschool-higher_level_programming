0eaaf9f40ad739c19429ae9ec0fa8b2974b152a8
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-13 22:25:32 -0500

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Comment Rectangle"""
+"""class Rectangle"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
+    """create a class called rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """init"""
