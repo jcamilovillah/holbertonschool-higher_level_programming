2be31aa25c7288ea94e0870b80d662f0a9c37a47
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-13 22:24:15 -0500

    rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+"""Comment Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """area"""
+        return self.__height * self.__width
+
+    def display(self):
+        """display"""
+        space = ""
+        for y in range(self.__y):
+            print()
+        for x in range(self.__x):
+            space += " "
+        for i in range(self.__height):
+            print(space, end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """str"""
+        return "[Rectangle] " + "(" + str(self.id) + ") "\
+            + str(self.__x) + "/" + str(self.__y) + " - " +\
+            str(self.__width) + "/" + str(self.__height)
+
+    def update(self, *args, **kwargs):
+        """update"""
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.__width = args[i]
+                if i == 2:
+                    self.__height = args[i]
+                if i == 3:
+                    self.__x = args[i]
+                if i == 4:
+                    self.__y = args[i]
+
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "width":
+                self.__width = value
+            if key == "height":
+                self.__height = value
+            if key == "x":
+                self.__x = value
+            if key == "y":
+                self.__y = value
+
+    def to_dictionary(self):
+        """dictionary"""
+        return dict(id=self.id, width=self.width,
+                    height=self.height, x=self.x, y=self.y)
