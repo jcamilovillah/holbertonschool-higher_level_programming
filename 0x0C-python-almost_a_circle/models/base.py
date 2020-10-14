dde3c6bd1d4bf15e05fe8a14a194b91fb666b9f4
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-13 20:39:10 -0500

    condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """json representation"""
-        if list_dictionaries is None or len(list_dictionaries) is 0:
+        list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
