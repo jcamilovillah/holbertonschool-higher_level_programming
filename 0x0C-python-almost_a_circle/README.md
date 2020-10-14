260e845e9255658b9ed9166f594400a603c6a1de
jcamilovillah <villahjuancamilo@gmail.com>
2020-10-13 22:13:27 -0500

    second chance

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+# Python 0x0C - Almost a circlejajas
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base"""
+
+
+class Base():
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
