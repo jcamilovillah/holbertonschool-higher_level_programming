26040cde345d9f4f1cc785d89540b07b4c4d8298
jcamilovillah <villahjuancamilo@gmail.com>
2020-11-17 19:20:07 -0500

    create user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NO EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
