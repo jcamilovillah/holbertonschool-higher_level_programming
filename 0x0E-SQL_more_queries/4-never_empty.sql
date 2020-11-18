75b9f1a129dec7d91844f82820aeff1b35347f74
jcamilovillah <villahjuancamilo@gmail.com>
2020-11-17 19:53:17 -0500

    default 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- script that creates the table id_not_null
-CREATE TABLE id_not_null IF NOT EXISTS (id int DEFAULT 1), name VARCHAR(256));
+CREATE TABLE id_not_null IF NOT EXISTS (id int DEFAULT 1), name VARCHAR(256);
