1d69b91df8d0598f411a71d943d4ec38567a0c42
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-11 20:57:34 +0000

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+"""  
+"""
+
+from sys import argv
+
+if __name__ == "__main__":
+
+    userr = argv[1]
+    passw = argv[2]
+    namd = argv[3]
+    match = argv[4]
+    lc = "localhost"
+
+    db = MySQLdb.connect(host=lc, port=3306, user=userr, passwd=passw, db=namd)
+    cur = db.cursor()
+    exe = cur.execute("SELECT * FROM states WHERE name=%s\
+                    ORDER BY id;", (match,))
+    for x in cur.fetchall():
+        print(x)
+    db.close()
+    cur.close()
