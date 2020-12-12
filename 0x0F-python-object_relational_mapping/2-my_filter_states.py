17590b7dfdfcb3482c3d798e046b30a131706162
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-12 18:28:16 +0000

    fix import

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states with a name starting with N (upper N)
+""" script that list states trought Mysqldb
 """
 
 
@@ -11,14 +11,13 @@ if __name__ == "__main__":
     userr = argv[1]
     passw = argv[2]
     namd = argv[3]
-    match = argv[4]
+    searched = argv[4]
     lc = "localhost"
 
     db = MySQLdb.connect(host=lc, port=3306, user=userr, passwd=passw, db=namd)
     cur = db.cursor()
-    exe = cur.execute("SELECT * FROM states\
-        WHERE name = '{}' ORDER BY id;".format(match))
-    print(match)
+    exe = cur.execute("SELECT * FROM states WHERE name='{}'\
+                ORDER BY id;".format(searched))
     for x in cur.fetchall():
         print(x)
     db.close()
