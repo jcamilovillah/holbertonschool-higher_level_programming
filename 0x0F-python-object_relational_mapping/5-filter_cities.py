12b76bdd6a9313a2f218c4594ebf30dbfe40fb5d
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-12 18:29:27 +0000

    fix import

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that list states
+""" script that lists all cities and states with left join
 """
 
 
@@ -11,18 +11,19 @@ if __name__ == "__main__":
     userr = argv[1]
     passw = argv[2]
     namd = argv[3]
-    state_name = argv[4]
+    sta_nme = argv[4]
     lc = "localhost"
 
     db = MySQLdb.connect(host=lc, port=3306, user=userr, passwd=passw, db=namd)
     cur = db.cursor()
     exe = cur.execute("SELECT cities.name FROM cities JOIN states\
-        ON cities.state_id=states.id\
-        WHERE states.name= % s ORDER BY cities.id", (state_name,))
-    comma = ""
+                ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id;", (sta_nme,))
+    sep = ""
     for row in cur.fetchall():
         for col in row:
-            print("{}{}".format(comma, col), end="")
-            comma = ", "
+            print("{}{}".format(sep, col), end="")
+            sep = ", "
+    print()
     db.close()
     cur.close()
