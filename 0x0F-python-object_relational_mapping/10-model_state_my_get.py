da5f8a9f119072b758a59e2d6e8c492712952116
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-12 16:20:04 +0000

    print states passed

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that lists all State objects that contain the letter a
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = argv[1]
+    passw = argv[2]
+    nm_dt = argv[3]
+    search = argv[4]
+    consult = False
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, nm_dt))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    consulta = session.query(State).filter(State.name == search)
+    for instance in consulta:
+        print("{}".format(instance.id))
+        consult = True
+    if consult is False:
+        print("Not found")
+    session.close()
