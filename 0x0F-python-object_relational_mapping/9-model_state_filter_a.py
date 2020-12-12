bb77099a4240945765f826a4baead9cc682c9fd8
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-12 18:19:54 +0000

    list contains a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that lists all State objects that contain the letter a
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = argv[1]
+    passw = argv[2]
+    nm_dt = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, passw, nm_dt))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    consulta = session.query(State).filter(State.name.ilike('%a%'))
+    for instance in consulta:
+        print("{}: {}".format(instance.id, instance.name))
+    session.close()
