f5d387d379cf68c1e024d5aac5c74c2e38085844
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-19 02:07:21 +0000

    sort list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,5 @@ def find_peak(list_of_integers):
     """find peak function"""
     if len(list_of_integers) == 0:
             return None
-    return (max(list_of_integers))
+    list_of_integers.sort()
+    return (list_of_integers[-1])
