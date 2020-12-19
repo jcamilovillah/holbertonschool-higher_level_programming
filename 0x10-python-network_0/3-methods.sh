34f194a6c384c5ac36edb9add96b01e51768f98b
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-19 00:35:30 +0000

    methods allow

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i Allow | awk '{for(i=2;i<=NF;i++){printf " %s", $i}; printf "\n"}'
+curl -sI "$1" | grep -i Allow | awk '{out=""; for(i=2;i<=NF;i++){out=out" "$i}; print out}'
