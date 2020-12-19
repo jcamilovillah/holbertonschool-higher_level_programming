93f82efccb0118120e972087265f4ed7483fd1ad
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-19 02:00:00 +0000

    interview

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+"""find peak function"""
+   if len(list_of_integers) == 0:
+        return None
+    return (max(list_of_integers))
