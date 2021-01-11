3040bef6eb800f214d6464f01301cd9a2771eb35
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-11 22:50:37 +0000

    requests pack

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
 
-    print("Body response: ")
+    print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
