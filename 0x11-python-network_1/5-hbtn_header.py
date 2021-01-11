675d87275690c5c1c3cf9ab36e82f9ed61552297
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-11 22:57:10 +0000

    header ez

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    dic = response.headers['X-Request-Id']
+    print(dic)
