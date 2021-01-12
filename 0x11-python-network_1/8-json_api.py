56f8e93f567759a48b9560cce1010066bf23e7fe
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-12 01:16:03 +0000

    json validate

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+    response = requests.post(
+        'http://0.0.0.0:5000/search_user', data={'q': q})
+    if response.json() == {}:
+        print("No result")
+    else:
+        try:
+            d = response.json()
+            print("[{}] {}".format(d.get("id"), d.get("name")))
+        except:
+            print("Not a valid JSON")
