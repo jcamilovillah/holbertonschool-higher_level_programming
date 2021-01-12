621d53edf9e4505995558471a8f0ada557ca42e8
jcamilovillah <villahjuancamilo@gmail.com>
2021-01-12 04:44:30 +0000

    set format print

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    response = requests.get('https://api.github.com/user', auth=(username, password))
+    response = requests.get('https://api.github.com/user',
+                            auth=(username, password))
     print(response.json().get("id"))
