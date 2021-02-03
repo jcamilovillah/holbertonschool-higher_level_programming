3c68d45916a95ffba9e5b3aa350290637ae2e993
jcamilovillah <villahjuancamilo@gmail.com>
2021-02-03 00:09:33 +0000

    add condition in 2 args case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
-if (process.argv.length < 3) {
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
   console.log('No argument');
 } else {
   console.log('Argument found');
