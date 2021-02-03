8261a04ffb2e5ec054e175d2d163517c2cc53883
jcamilovillah <villahjuancamilo@gmail.com>
2021-02-03 03:45:32 +0000

    change datatype of array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  let arrSort = process.argv.sort((a, b) => a - b);
+  const arrSort = process.argv.sort((a, b) => a - b);
   console.log(arrSort[arrSort.length - 2]);
 }
