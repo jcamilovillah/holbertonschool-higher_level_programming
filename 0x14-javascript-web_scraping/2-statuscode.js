3c98866146c45fc3680fcd407f166b75423d6270
jcamilovillah <villahjuancamilo@gmail.com>
2021-02-09 19:58:30 +0000

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2]
+request(url, function (error, response, body) {
+  console.log('code: %s', response.statusCode);
+});
