b62f5364fda80126075861da103a50d343b34f10
jcamilovillah <villahjuancamilo@gmail.com>
2021-02-10 03:24:46 +0000

    script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,16 +7,14 @@ request(url, function (err, response, body) {
   }
   const json = JSON.parse(body);
   const dict = {};
-  let count = 1;
-  let primero = json[0].userId;
   for (const userd of json) {
     const saveId = userd.userId;
     if (userd.completed) {
-      if (primero !== userd.userId) {
-        primero = userd.userId;
-        count = 1;
+      if (dict[saveId]) {
+        dict[saveId]++;
+      } else {
+        dict[saveId] = 1;
       }
-      dict[saveId] = count++;
     }
   }
   console.log(dict);
